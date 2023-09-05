e6ed78438fd92ac72566e4cb1f44169d19a23c88
P4risGreen <hokagen.hp@gmail.com>
2023-09-05 22:49:14 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println("数字は10以上か:");
+        int i = 100;
+        if(i >= 100){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+            
+        }
+    }
+}
